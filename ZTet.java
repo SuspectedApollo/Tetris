3c322f88f89d9f03875b40af69fd25be0f62d87b
David Shelton <sheltondman@gmail.com>
2016-04-20 17:05:03 -0400

    Corrected spelling

--- ZTet.java
+++ ZTet.java
@@ -2,7 +2,7 @@ package tetris;
 
 import java.awt.Point;
 
-public class ZTet extends Tetronimo{
+public class ZTet extends Tetromino{
 	
 	protected Point[][] COORDS = { {new Point(0,2), new Point (1,2), new Point(1,1), new Point(2,1)},
 								   {new Point(2,2), new Point (2,1), new Point(1,1), new Point(1,0)},
