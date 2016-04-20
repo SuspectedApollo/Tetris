b651734a95f0b5a3e82ff4ff126c18c132cbca24
David Shelton <sheltondman@gmail.com>
2016-04-20 16:48:15 -0400

    Added Z and S Tetrominos

---
+++ STet.java
@@ -0,0 +1,17 @@
+package tetris;
+
+import java.awt.Point;
+
+public class STet extends Tetronimo{
+	
+	protected Point[][] COORDS = { {new Point(0,1), new Point (1,1), new Point(1,2), new Point(2,2)},
+								   {new Point(2,0), new Point (2,1), new Point(1,1), new Point(1,2)},
+							       {new Point(0,0), new Point (1,0), new Point(1,1), new Point(2,1)},
+								   {new Point(1,0), new Point (1,1), new Point(0,1), new Point(0,2)}}; 
+			
+	protected int COLOR = 4;
+	
+	public STet(int[][] grid){
+		super(grid);
+	}
+}
---
+++ ZTet.java
@@ -0,0 +1,17 @@
+package tetris;
+
+import java.awt.Point;
+
+public class ZTet extends Tetronimo{
+	
+	protected Point[][] COORDS = { {new Point(0,2), new Point (1,2), new Point(1,1), new Point(2,1)},
+								   {new Point(2,2), new Point (2,1), new Point(1,1), new Point(1,0)},
+							       {new Point(0,1), new Point (1,1), new Point(1,0), new Point(2,0)},
+								   {new Point(0,0), new Point (0,1), new Point(1,1), new Point(1,2)}}; 
+			
+	protected int COLOR = 5;
+	
+	public ZTet(int[][] grid){
+		super(grid);
+	}
+}
